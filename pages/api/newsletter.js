16435674b4f6ec6844919fede83e6c80644e0757
kubicsnft <110540165+kubicsnft@users.noreply.github.com>
2022-08-03 17:42:31 +0200

    Update email error newsletter
    info@kubicsnft.com

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -50,7 +50,7 @@ export default async (req, res) => {
         return res.status(201).json({ error: null })
     } catch (error) {
         return res.status(400).json({
-            error: `Oops, something went wrong... Send me an email at xxx@gmail.com and I'll add you to the list.`,
+            error: `Oops, something went wrong... Send me an email at info@kubicsnft.com and I'll add you to the list.`,
         })
 
         // Report error to Sentry or whatever
